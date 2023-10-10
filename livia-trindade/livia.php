5aa2b1c7d37f7a23d034f6fc3b2b7ac865083846
Lívia Trindade <145488817+livia-trindade@users.noreply.github.com>
2023-10-10 16:17:36 -0300

    Add files via upload

---
+++ livia-trindade/livia.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo 'impressão em php';
+echo "<br>";
+print_r (" isto é uma função em php");
+?>
+
+<li>
+    <ul>elemento1</ul>
+    <ul>elemento2</ul>
+</li>
+
+<?php
+echo "hello world";
+?>
+
+</body>
+</html>
+
